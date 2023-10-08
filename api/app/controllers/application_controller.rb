8c0f09e90339d874e60a7a95f4545bd9a912ab80
kenny-kento <kento.nishibe@divx.co.jp>
2023-10-08 15:59:36 +0900

    トークンベースの認証の際の不用な部分の削除・修正

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
-        skip_before_action :verify_authenticity_token
         before_action :configure_permitted_parameters, if: :devise_controller?
 
         def configure_permitted_parameters
